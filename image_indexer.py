944f28b256622a4fb83c21e965b030272fcdebd5
Christian VanMeter <christian.vanmeter@yahoo.com>
2026-01-30 12:52:34 -0600

    indexer

--- image_indexer.py
+++ image_indexer.py
@@ -6,3 +6,8 @@ from sentence_transformers import SentenceTransformer
 import pickle
 from io import BytesIO
 
+#load clip model
+model = SentenceTransformer('clip-ViT-B-32')
+
+#load excel files
+all_files = glob.glob("data/*.xlsx")
